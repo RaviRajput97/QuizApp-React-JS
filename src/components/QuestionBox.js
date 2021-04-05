0c4b18b62e68fe2435c62d0f6703ac86b7c8955e
Ravi Rajput <43280854+RaviRajput97@users.noreply.github.com>
2021-04-06 02:08:31 +0530

    All files uploaded

---
+++ src/components/QuestionBox.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+//create simple JS function that brings props using a single argumrnt 
+const QuestionBox = ({question, options, selected}) => {
+  const [answer, setAnswer] = useState(options);
+  return (
+    <div className="questionBox">
+      <div className="question">{question}</div>
+      {answer.map((text, index) => (
+        <button
+          key={index}
+          className="answerBtn"
+          onClick={() => {
+            setAnswer([text]);
+            selected(text);
+          }}
+        >
+          {text}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default QuestionBox;
